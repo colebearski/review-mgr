aa855443c4bf16ddadb67fd9a85e4f054baddaf3
Cole Pedersen <cole.pedersen@gmail.com>
2018-12-11 20:39:45 -0700

    Updates

---
+++ precache-manifest.e29fa5b1d8095172e851f7246a3d4ce8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b84cc87ee13ae0701500",
+    "url": "/review-mgr/static/js/runtime~main.b84cc87e.js"
+  },
+  {
+    "revision": "36580f65cca06db3a913",
+    "url": "/review-mgr/static/js/main.36580f65.chunk.js"
+  },
+  {
+    "revision": "4a51a6ddb1ebaa178a89",
+    "url": "/review-mgr/static/js/1.4a51a6dd.chunk.js"
+  },
+  {
+    "revision": "36580f65cca06db3a913",
+    "url": "/review-mgr/static/css/main.b18b303e.chunk.css"
+  },
+  {
+    "revision": "4a51a6ddb1ebaa178a89",
+    "url": "/review-mgr/static/css/1.5fd78e28.chunk.css"
+  },
+  {
+    "revision": "5d666a3ce358cfb55bae594183653399",
+    "url": "/review-mgr/index.html"
+  }
+];
